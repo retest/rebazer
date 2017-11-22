617bb921ae337298b5115c813885652f92780a91
Joe <modulo11@users.noreply.github.com>
2017-11-22 10:38:28 +0100

    Consistently name rebazer classes

--- src/main/java/org/retest/rebazer/RebazerApplication.java
+++ src/main/java/org/retest/rebazer/RebazerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ReBaZerApplication {
+public class RebazerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ReBaZerApplication.class, args);
+		SpringApplication.run(RebazerApplication.class, args);
 	}
 }
