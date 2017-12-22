22ad8af8a5f0edec0c485ea1899ba45c806f35c1
Daniel Kraus <daniel.kraus@retest.de>
2017-12-22 23:39:03 +0100

    Revert "Make PullRequest immutable"
    @Data in combination with final fields already produces an immutable class (see generated code, no setters).

--- src/main/java/org/retest/rebazer/domain/PullRequest.java
+++ src/main/java/org/retest/rebazer/domain/PullRequest.java
@@ -1,13 +1,10 @@
 package org.retest.rebazer.domain;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Setter;
 
 @Data
 @Builder
-@Setter( AccessLevel.NONE )
 public class PullRequest {
 
 	private final Integer id;
