81c8c433b6045f634d183c71a8ea09f288f45356
Daniel Kraus <daniel.kraus@retest.de>
2017-12-23 00:18:53 +0100

    Fix some typos and improve naming

--- src/test/java/org/retest/rebazer/service/PullRequestLastUpdateStoreTest.java
+++ src/test/java/org/retest/rebazer/service/PullRequestLastUpdateStoreTest.java
@@ -13,7 +13,7 @@ public class PullRequestLastUpdateStoreTest {
 
 	Repository repo;
 	PullRequest pr;
-	String dateLastUpdate;
+	String lastUpdate;
 
 	PullRequestLastUpdateStore cut;
 
@@ -24,30 +24,30 @@ public class PullRequestLastUpdateStoreTest {
 		repo = mock( Repository.class );
 		pr = mock( PullRequest.class );
 		when( pr.getId() ).thenReturn( 1 );
-		dateLastUpdate = "2017-11-30T10:05:28+00:00";
-		when( pr.getLastUpdate() ).thenReturn( dateLastUpdate );
+		lastUpdate = "2017-11-30T10:05:28+00:00";
+		when( pr.getLastUpdate() ).thenReturn( lastUpdate );
 	}
 
 	@Test
-	public void isHandled_for_unknown_should_return_false() {
+	public void isHandled_should_return_false_for_unknown_pr() {
 		assertThat( cut.isHandled( repo, pr ) ).isFalse();
 	}
 
 	@Test
-	public void isHandled_should_return_true_if_is_handled_before() {
+	public void isHandled_should_return_true_if_pr_is_handled_before() {
 		cut.setHandled( repo, pr );
 		assertThat( cut.isHandled( repo, pr ) ).isTrue();
 	}
 
 	@Test
-	public void isHandled_should_return_false_if_is_handled_but_repo_is_reseted_before() {
+	public void isHandled_should_return_false_if_pr_is_handled_but_repo_is_reset_before() {
 		cut.setHandled( repo, pr );
 		cut.resetAllInThisRepo( repo );
 		assertThat( cut.isHandled( repo, pr ) ).isFalse();
 	}
 
 	@Test
-	public void isHandled_should_return_false_if_pullrequest_did_change() throws Exception {
+	public void isHandled_should_return_false_if_pr_did_change() throws Exception {
 		cut.setHandled( repo, pr );
 
 		final String newDate = "2017-11-30T10:22:55+00:00";
@@ -57,14 +57,14 @@ public class PullRequestLastUpdateStoreTest {
 	}
 
 	@Test
-	public void getLastDate_for_unknown_should_return_null() {
+	public void getLastDate_should_return_null_for_unknown_pr() {
 		assertThat( cut.getLastDate( repo, pr ) ).isNull();
 	}
 
 	@Test
-	public void getLastDate_should_return_the_date_if_is_handled_before() {
+	public void getLastDate_should_return_the_date_if_pr_is_handled_before() {
 		cut.setHandled( repo, pr );
-		assertThat( cut.getLastDate( repo, pr ) ).isEqualTo( dateLastUpdate );
+		assertThat( cut.getLastDate( repo, pr ) ).isEqualTo( lastUpdate );
 	}
 
 }
