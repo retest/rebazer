5510b65d08859a51d4b6e543a2d391f39db6dc18
Jakob Herdt <jakob.herdt@retest.de>
2018-01-24 12:28:55 +0100

    Change  templates to support multiple user

--- src/main/java/org/retest/rebazer/config/BitbucketConfig.java
+++ src/main/java/org/retest/rebazer/config/BitbucketConfig.java
@@ -1,28 +1,23 @@
 package org.retest.rebazer.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
 public class BitbucketConfig {
 
 	private final static String baseUrlV1 = "https://api.bitbucket.org/1.0";
 	private final static String baseUrlV2 = "https://api.bitbucket.org/2.0";
 
-	@Autowired
-	private RebazerConfig config;
-
 	@Bean
-	public RestTemplate bitbucketLegacyTemplate( final RestTemplateBuilder builder ) {
-		return builder.basicAuthorization( config.getUser(), config.getPass() ).rootUri( baseUrlV1 ).build();
+	public RestTemplate bitbucketLegacyTemplate( final RestTemplateBuilder builder, final String user,
+			final String pass ) {
+		return builder.basicAuthorization( user, pass ).rootUri( baseUrlV1 ).build();
 	}
 
 	@Bean
-	public RestTemplate bitbucketTemplate( final RestTemplateBuilder builder ) {
-		return builder.basicAuthorization( config.getUser(), config.getPass() ).rootUri( baseUrlV2 ).build();
+	public RestTemplate bitbucketTemplate( final RestTemplateBuilder builder, final String user, final String pass ) {
+		return builder.basicAuthorization( user, pass ).rootUri( baseUrlV2 ).build();
 	}
 
 }
--- src/main/java/org/retest/rebazer/config/GithubConfig.java
+++ src/main/java/org/retest/rebazer/config/GithubConfig.java
@@ -1,21 +1,16 @@
 package org.retest.rebazer.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
 public class GithubConfig {
 
 	private final static String baseUrl = "https://api.github.com/";
 
-	@Autowired
-	private RebazerConfig config;
-
 	@Bean
-	public RestTemplate githubTemplate( final RestTemplateBuilder builder ) {
-		return builder.basicAuthorization( config.getUser(), config.getPass() ).rootUri( baseUrl ).build();
+	public RestTemplate githubTemplate( final RestTemplateBuilder builder, final String user, final String pass ) {
+		return builder.basicAuthorization( user, pass ).rootUri( baseUrl ).build();
 	}
+
 }
