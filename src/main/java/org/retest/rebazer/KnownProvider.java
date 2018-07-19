15bee35c24da79cd1356ecd28ef95acf39071601
Martin Vietz <git@martin.vietz.eu>
2018-07-19 19:02:18 +0200

    Better variable names and RepositoryConnector selction to KnownProvider

--- src/main/java/org/retest/rebazer/KnownProvider.java
+++ src/main/java/org/retest/rebazer/KnownProvider.java
@@ -1,8 +1,27 @@
 package org.retest.rebazer;
 
+import org.retest.rebazer.config.RebazerConfig.RepositoryConfig;
+import org.retest.rebazer.config.RebazerConfig.Team;
+import org.retest.rebazer.connector.BitbucketConnector;
+import org.retest.rebazer.connector.GithubConnector;
+import org.retest.rebazer.connector.RepositoryConnector;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
 public enum KnownProvider {
 
 	BITBUCKET,
 	GITHUB;
+
+	public RepositoryConnector getRepository( final Team team, final RepositoryConfig repo,
+			final RestTemplateBuilder builder ) {
+		switch ( this ) {
+			case BITBUCKET:
+				return new BitbucketConnector( team, repo, builder );
+			case GITHUB:
+				return new GithubConnector( team, repo, builder );
+			default:
+				throw new RuntimeException( "No Repository defined for provider: " + this );
+		}
+	}
 
 }
--- src/main/java/org/retest/rebazer/RebazerService.java
+++ src/main/java/org/retest/rebazer/RebazerService.java
@@ -7,8 +7,6 @@ import org.retest.rebazer.config.RebazerConfig;
 import org.retest.rebazer.config.RebazerConfig.RepositoryConfig;
 import org.retest.rebazer.config.RebazerConfig.RepositoryHost;
 import org.retest.rebazer.config.RebazerConfig.Team;
-import org.retest.rebazer.connector.BitbucketConnector;
-import org.retest.rebazer.connector.GithubConnector;
 import org.retest.rebazer.connector.RepositoryConnector;
 import org.retest.rebazer.domain.PullRequest;
 import org.retest.rebazer.service.PullRequestLastUpdateStore;
@@ -31,64 +29,54 @@ public class RebazerService {
 	private final PullRequestLastUpdateStore pullRequestLastUpdateStore;
 
 	private final RestTemplateBuilder builder;
-	private RepositoryConnector provider;
 
 	@Scheduled( fixedDelayString = "${" + POLL_INTERVAL_KEY + ":" + POLL_INTERVAL_DEFAULT + "}000" )
 	public void pollToHandleAllPullRequests() {
 		config.getHosts().forEach( host -> {
 			host.getTeams().forEach( team -> {
-				team.getRepos().forEach( repo -> {
-					handleRepo( host, team, repo );
+				team.getRepos().forEach( repoConfig -> {
+					handleRepo( host, team, repoConfig );
 				} );
 			} );
 		} );
 	}
 
-	private void handleRepo( final RepositoryHost host, final Team team, final RepositoryConfig repo ) {
-		log.debug( "Processing {}.", repo );
-		switch ( host.getType() ) {
-			case BITBUCKET:
-				provider = new BitbucketConnector( team, repo, builder );
-				break;
-			case GITHUB:
-				provider = new GithubConnector( team, repo, builder );
-				break;
-			default:
-				log.info( "The hosting Service via: {} is not supported", host.getType() );
+	private void handleRepo( final RepositoryHost host, final Team team, final RepositoryConfig repoConfig ) {
+		log.debug( "Processing {}.", repoConfig );
+		final RepositoryConnector repoConnector = host.getType().getRepository( team, repoConfig, builder );
+		for ( final PullRequest pullRequest : repoConnector.getAllPullRequests( repoConfig ) ) {
+			handlePullRequest( repoConnector, repoConfig, pullRequest );
 		}
-		for ( final PullRequest pr : provider.getAllPullRequests( repo ) ) {
-			handlePR( provider, repo, pr );
-		}
-		log.debug( "Processing done for {}.", repo );
+		log.debug( "Processing done for {}.", repoConfig );
 	}
 
-	public void handlePR( final RepositoryConnector provider, final RepositoryConfig repositories,
+	public void handlePullRequest( final RepositoryConnector provider, final RepositoryConfig repoConfig,
 			final PullRequest pullRequest ) {
 		log.debug( "Processing {}.", pullRequest );
 
-		if ( pullRequestLastUpdateStore.isHandled( repositories, pullRequest ) ) {
+		if ( pullRequestLastUpdateStore.isHandled( repoConfig, pullRequest ) ) {
 			log.info( "{} is unchanged since last run (last change: {}).", pullRequest,
-					pullRequestLastUpdateStore.getLastDate( repositories, pullRequest ) );
+					pullRequestLastUpdateStore.getLastDate( repoConfig, pullRequest ) );
 
 		} else if ( !provider.greenBuildExists( pullRequest ) ) {
 			log.info( "Waiting for green build of {}.", pullRequest );
-			pullRequestLastUpdateStore.setHandled( repositories, pullRequest );
+			pullRequestLastUpdateStore.setHandled( repoConfig, pullRequest );
 
 		} else if ( provider.rebaseNeeded( pullRequest ) ) {
-			if ( !rebaseService.rebase( repositories, pullRequest ) ) {
+			if ( !rebaseService.rebase( repoConfig, pullRequest ) ) {
 				provider.addComment( pullRequest );
 			}
 			// we need to update the "lastUpdate" of a PullRequest to counteract if addComment is called
-			pullRequestLastUpdateStore.setHandled( repositories, provider.getLatestUpdate( pullRequest ) );
+			pullRequestLastUpdateStore.setHandled( repoConfig, provider.getLatestUpdate( pullRequest ) );
 
 		} else if ( !provider.isApproved( pullRequest ) ) {
 			log.info( "Waiting for approval of {}.", pullRequest );
-			pullRequestLastUpdateStore.setHandled( repositories, pullRequest );
+			pullRequestLastUpdateStore.setHandled( repoConfig, pullRequest );
 
 		} else {
 			log.info( "Merging pull request " + pullRequest );
 			provider.merge( pullRequest );
-			pullRequestLastUpdateStore.resetAllInThisRepo( repositories );
+			pullRequestLastUpdateStore.resetAllInThisRepo( repoConfig );
 		}
 	}
 
