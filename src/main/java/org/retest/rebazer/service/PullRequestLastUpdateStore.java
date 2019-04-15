9d186473ee62b0304409d99491e58f4078d37b88
IgorAlbert <igor.albert@retest.de>
2019-04-15 17:10:00 +0200

    Change String type of the timestamps to Data

--- src/main/java/org/retest/rebazer/service/PullRequestLastUpdateStore.java
+++ src/main/java/org/retest/rebazer/service/PullRequestLastUpdateStore.java
@@ -1,5 +1,7 @@
 package org.retest.rebazer.service;
 
+import java.time.OffsetDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,14 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class PullRequestLastUpdateStore {
 
-	private final Map<RepositoryConfig, Map<Integer, String>> pullRequestUpdateStates = new HashMap<>();
+	private final Map<RepositoryConfig, Map<Integer, Date>> pullRequestUpdateStates = new HashMap<>();
 
 	public void setHandled( final RepositoryConfig repoConfig, final PullRequest pullRequest ) {
 		getMapFor( repoConfig ).put( pullRequest.getId(), pullRequest.getLastUpdate() );
 	}
 
-	public String getLastDate( final RepositoryConfig repoConfig, final PullRequest pullRequest ) {
-		return getMapFor( repoConfig ).get( pullRequest.getId() );
+	public Date getLastDate( final RepositoryConfig repoConfig, final PullRequest pullRequest ) {
+		return getMapFor( repoConfig ).getOrDefault( pullRequest.getId(),
+				Date.from( OffsetDateTime.parse( "2019-01-01T00:00:00Z" ).toInstant() ) );
 	}
 
 	public void resetAllInThisRepo( final RepositoryConfig repoConfig ) {
@@ -25,10 +28,10 @@ public class PullRequestLastUpdateStore {
 	}
 
 	public boolean isHandled( final RepositoryConfig repoConfig, final PullRequest pullRequest ) {
-		return pullRequest.getLastUpdate().equals( getLastDate( repoConfig, pullRequest ) );
+		return pullRequest.getLastUpdate().compareTo( getLastDate( repoConfig, pullRequest ) ) == 0;
 	}
 
-	private Map<Integer, String> getMapFor( final RepositoryConfig repoConfig ) {
+	private Map<Integer, Date> getMapFor( final RepositoryConfig repoConfig ) {
 		return pullRequestUpdateStates.computeIfAbsent( repoConfig, key -> new HashMap<>() );
 	}
 
