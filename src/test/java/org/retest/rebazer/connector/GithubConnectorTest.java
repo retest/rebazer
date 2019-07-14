f8dcdd5e386e8b00555db0f036fd552fc402ad74
IgorAlbert <igor.albert@retest.de>
2019-07-14 18:31:52 +0200

    The values should be null

--- src/test/java/org/retest/rebazer/connector/GithubConnectorTest.java
+++ src/test/java/org/retest/rebazer/connector/GithubConnectorTest.java
@@ -107,13 +107,13 @@ class GithubConnectorTest {
 
 	private static Stream<Arguments> reviewStates() {
 		return Stream.of( //
-				Arguments.of( "\"\"", "\"\"", "\"\"", false, false ),
-				Arguments.of( "\"\"", "\"\"", "\"\"", true, false ),
+				Arguments.of( null, null, null, false, false ), //
+				Arguments.of( null, null, null, true, false ),
 				Arguments.of( "APPROVED", "COMMENTED", "COMMENTED", false, true ),
-				Arguments.of( "APPROVED", "COMMENTED", "\"\"", true, false ),
-				Arguments.of( "CHANGES_REQUESTED", "COMMENTED", "\"\"", true, false ),
+				Arguments.of( "APPROVED", "COMMENTED", null, true, false ),
+				Arguments.of( "CHANGES_REQUESTED", "COMMENTED", null, true, false ),
 				Arguments.of( "APPROVED", "CHANGES_REQUESTED", "APPROVED", true, false ),
-				Arguments.of( "APPROVED", "COMMENTED", "\"\"", true, false ),
+				Arguments.of( "APPROVED", "COMMENTED", null, true, false ),
 				Arguments.of( "APPROVED", "APPROVED", "APPROVED", true, true ) );
 	}
 
